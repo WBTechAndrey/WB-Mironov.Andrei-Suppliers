a705d647b6043604cf85989767b6d5ac00fbd422
Andrey <poiprizamoda@gmail.com>
2024-06-14 21:55:46 +0300

    feat: setup redux store

--- src/components/ErrorBoundary/ErrorBoundary.tsx
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -1,4 +1,4 @@
-import { Component, ErrorInfo, ReactNode } from "react";
+import React, { Component, ErrorInfo, ReactNode } from "react";
 import { Button } from "../common/Button";
 import "./index.scss";
 import { getAllCookieNames } from "../../helpers/getAllCookieNames";
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import {ErrorBoundary} from "./components/ErrorBoundary/ErrorBoundary";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import { ErrorBoundary } from "./components/ErrorBoundary/ErrorBoundary";
+import { setupStore } from "./store/store";
 
 const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
+
+const store = setupStore();
+
 root.render(
+  <Provider store={store}>
     <ErrorBoundary>
-        <App/>
+      <App />
     </ErrorBoundary>
+  </Provider>,
 );
